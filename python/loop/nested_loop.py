4f03482ec3d5657591b4d992d68f3087ae9488e0
Your Namebelovetech <belovetech@gmail.com>
2022-05-26 06:24:33 +0000

    add nested loop and pattern.py

---
+++ python/loop/nested_loop.py
@@ -0,0 +1,11 @@
+i = 1
+
+while i <= 3:
+    print(f'Outer loop iteration {i}')
+    j = 1
+    while j <= 3:
+         print(f'Inner loop iteration {j}')
+         j +=1
+    print('_________________________')
+    i +=1
+         
---
+++ python/loop/pattern.py
@@ -0,0 +1,9 @@
+#  print * (5 x 5)
+i = 1
+while i <= 5:
+    j = 1
+    while j <= 5:
+        print('*', end=' ')
+        j += 1
+    print()
+    i += 1
