582b1ec526c939a5f989a4141cdf644fd0db0bfd
Your Namebelovetech <belovetech@gmail.com>
2022-05-27 11:49:07 +0000

    Module in python

---
+++ python/module/module.py
@@ -0,0 +1,14 @@
+import util
+import util as u
+from util import negative_or_positive
+
+# import util
+util.even_or_odd(10)
+util.negative_or_positive(0)
+
+# import util as u
+u.even_or_odd(3)
+u.negative_or_positive(-1)
+
+# from util import negative_or_positive
+negative_or_positive(1)
---
+++ python/module/util.py
@@ -0,0 +1,14 @@
+def even_or_odd(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
+def negative_or_positive(num):
+    if num == 0:
+        print("Neither Positive Nor Negative")
+    elif num < 0:
+        print("Negative")
+    else:
+        print("Positive")
