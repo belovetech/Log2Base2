df1f11fab0febbfd1c9416e2745ce5e0e283b8c7
Your Namebelovetech <belovetech@gmail.com>
2022-05-28 05:32:03 +0000

    Refactor the code into a function

--- problem_solving_for_beginners/python/armstrong_number.py
+++ problem_solving_for_beginners/python/armstrong_number.py
@@ -1,15 +1,20 @@
+# armstrong_number - checks if a number is armstrong number or not
+
+def armStrong(num):
+    sum = 0
+    tmp = num
+
+    while num >= 1:
+        mod = num % 10
+        sum = sum + mod * mod * mod
+        num = num // 10
+
+    if sum == tmp:
+        print("Armstrong Number")
+    else:
+        print("Not Armstrong Number")
+
+
 num = int(input("Enter a positive number: "))
 #write your code here
-sum = 0
-tmp = num
-
-while num >= 1:
-    mod = num % 10
-    sum = sum + mod * mod * mod
-    num = num // 10
-
-print(sum, tmp)
-if sum == tmp:
-    print("Armstrong Number")
-else:
-    print("Not Armstrong Number")
+armStrong(num)
---
+++ problem_solving_for_beginners/python/fibonnaci.py
@@ -0,0 +1,16 @@
+n = int(input("Enter a number: "))
+#write your code here
+a = 0
+b = 1
+
+for i in range (0, n, 1):
+    if i == 0:
+        print(i, end=" ")
+    elif i == 1:
+        print(i, end=" ")
+    else:
+        c = a + b
+        print(c, end=" ")
+        a = b
+        b = c
+print()
