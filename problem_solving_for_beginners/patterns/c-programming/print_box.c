31310f1d6aaa64a8c82c0bde69674d02061f022d
Your Namebelovetech <belovetech@gmail.com>
2022-05-28 20:04:21 +0000

    print pattern

---
+++ problem_solving_for_beginners/patterns/c-programming/print_box.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - prints n x n box pattern
+ * 
+ *  Return: 0 always (success)
+ */
+ 
+int main(void)
+{
+    int n, i, j;
+    
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
