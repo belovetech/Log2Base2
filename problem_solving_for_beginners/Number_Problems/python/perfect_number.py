3e3dee7a4f46077fd31538e672dfea80979216c1
Your Namebelovetech <belovetech@gmail.com>
2022-05-28 12:21:05 +0000

    Check for edge case

--- problem_solving_for_beginners/Number_Problems/python/perfect_number.py
+++ problem_solving_for_beginners/Number_Problems/python/perfect_number.py
@@ -1,16 +1,15 @@
 import math
 
 def isPerfect(n):
-    if n < 1:
-        print("Enter a positive number")
-    else:
-        sqr = int(math.sqrt(n)) + 1
-        res = 1
-        for i in range (2, sqr):
-            if n % i == 0:
-                tmp = n // i
-                res += i + tmp
-        return res
+    root = int(math.sqrt(n))
+    res = 1
+    for i in range (2, root+1):
+        if n % i == 0:
+            if n // i == i:
+                res = res + i
+            else:
+                res = res + i + n // i
+    return res
 
 
 n = int(input("Enter a positive number: "))
