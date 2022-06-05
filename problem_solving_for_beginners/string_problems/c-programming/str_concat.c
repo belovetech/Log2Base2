db8943d10b93d9c58aac722416ca5501fc765b8f
Your Namebelovetech <belovetech@gmail.com>
2022-06-05 19:34:19 +0000

    Modified the length tracking

--- problem_solving_for_beginners/string_problems/c-programming/str_concat.c
+++ problem_solving_for_beginners/string_problems/c-programming/str_concat.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
 /**
  * main - Concatenates two strings
- * 
- * Return: 0 always 
+ *
+ * Return: 0 always (success)
  */
 int main(void)
 {
     char str1[10], str2[10], str3[20];
-    
+
     printf("Enter two strings: ");
     scanf("%s%s", str1, str2);
-    
-    int i, j;
-    
+
+    int i, len = 0;
+
     for (i = 0; str1[i] != '\0'; i++)
-        str3[i] = str1[i];
-        
-    for (j = 0; str2[j] != '\0'; j++)
-        str3[i+j] = str2[j];
-        
-    str3[i+j] = '\0';
-    
-    printf("%s", str3);
-    
+        str3[len++] = str1[i];
+
+    for (i = 0; str2[i] != '\0'; i++)
+        str3[len++] = str2[i];
+
+    str3[len] = '\0';
+
+    printf("%s\n", str3);
+
     return 0;
 }
-    
