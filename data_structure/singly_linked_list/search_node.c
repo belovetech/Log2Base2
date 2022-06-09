8734e5b96469be16b4d3585652dd7f7ecd918013
Your Namebelovetech <belovetech@gmail.com>
2022-06-09 19:04:10 +0000

    Search node element

--- data_structure/singly_linked_list/search_node.c
+++ data_structure/singly_linked_list/search_node.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+int search_node(struct node *head, int key)
+{
+    struct node *tmp;
+    int flag = 0;
+
+    tmp = head;
+    while (tmp != NULL)
+    {
+        if (tmp->data == key)
+        {
+            flag = 1;
+            break;
+        }
+        tmp = tmp->next;
+    }
+    return (flag);
+}
