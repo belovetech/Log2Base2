d3418a96dafe2216f02118c618bfe5a3378344e2
Your Namebelovetech <belovetech@gmail.com>
2022-06-09 18:29:59 +0000

    Move other function to a sepearte function

--- data_structure/singly_linked_list/insert_at_beginning.c
+++ data_structure/singly_linked_list/insert_at_beginning.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct node
-{
-    int data;
-    struct node *next;
-};
+#include "lists.h"
 
 /**
  * insert_at_beginning - insert data to singly linked list
@@ -28,37 +21,4 @@ struct node *insert_at_beginning(struct node **head, int val)
      *(head) = newNode;
 
      return (newNode);
-}
-
-/**
- * print_node - print singly linked list data
- *
- * @head: pointer to the head
- */
-int print_node(struct node *head)
-{
-    struct node *tmp = head;
-    int counter = 0;
-
-    while (tmp != NULL)
-    {
-        printf("%d\n", tmp->data);
-        tmp = tmp->next;
-        counter++;
-    }
-    return (counter);
-}
-
-int main(void)
-{
-    struct node *head = NULL;
-
-    insert_at_beginning(&head, 10);
-    insert_at_beginning(&head, 20);
-    insert_at_beginning(&head, 30);
-    insert_at_beginning(&head, 40);
-    insert_at_beginning(&head, 50);
-
-    print_node(head);
-    return (0);
 }
