aa268160f26a94ca17d0552b291a193535e4b378
Your Namebelovetech <belovetech@gmail.com>
2022-06-09 18:35:39 +0000

    Refactor the code

--- data_structure/singly_linked_list/insert_at_end.c
+++ data_structure/singly_linked_list/insert_at_end.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-struct node
-{
-    int data;
-    struct node *next;
-};
+/**
+ * insert_at_end - insert data at end of singly linked list
+ * @head: pointer to pointer
+ * @val: data to add.
+ *
+ * Return: pointer to new node
+ */
 
 struct node *insert_at_end(struct node **head, int val)
 {
@@ -33,36 +34,4 @@ struct node *insert_at_end(struct node **head, int val)
     lastNode->next = newNode;
 
     return (newNode);
-}
-
-
-/**
- * print_node - print singly linked list data
- *
- * @head: pointer to the head
- */
-int print_node(struct node *head)
-{
-    struct node *tmp = head;
-    int counter = 0;
-
-    while (tmp != NULL)
-    {
-        printf("%d\n", tmp->data);
-        tmp = tmp->next;
-        counter++;
-    }
-    return (counter);
-}
-
-int main(void)
-{
-    struct node *head = NULL;
-    
-    insert_at_end(&head, 20);
-    insert_at_end(&head, 30);
-    insert_at_end(&head, 40);
-
-    print_node(head);
-    return (0);
 }
