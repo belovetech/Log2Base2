2f9cfbede81729a6701cb6908b88b82dcd4aae90
Your Namebelovetech <belovetech@gmail.com>
2022-06-25 12:45:19 +0000

    Find equal or unequal of two integers

--- problem_solving_for_beginners/bitwise/python/find_equal.py
+++ problem_solving_for_beginners/bitwise/python/find_equal.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+"""Finds equal or not equal using bitwise XOR operator"""
 
-num1, num2 = input("Enter two positive integers").split("")
+num1, num2 = input("Enter two positive integers: ").split()
 
-# num2 = int(input())
-#write your code here
-
-if (int(num1) ^ int(num2()) == 0:
+if (int(num1) ^ int(num2)) == 0:
     print("Equal")
 else:
     print("Unequal")
