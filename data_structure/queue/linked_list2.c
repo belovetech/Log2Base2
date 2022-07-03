9a332f422ec2ab4ef2184b49b5ede81335810e3d
Your Namebelovetech <belovetech@gmail.com>
2022-07-03 20:10:04 +0000

    Second implementation of queue using linked list

--- data_structure/queue/linked_list2.c
+++ data_structure/queue/linked_list2.c
@@ -13,19 +13,17 @@ struct node *front = NULL, *rear = NULL;
  * enqueue - Adds element to the queue
  * @val: Value to add
  */
- 
-
-void enqueue1(int val)
+void enqueue(int val)
 {
 	struct node *newNode;
-	
+
 	newNode =  malloc(sizeof(struct node));
 	if (newNode == NULL)
 		return;
-	
+
 	newNode->data = val;
 	newNode->next = NULL;
-	
+
 	if (front == NULL && rear == NULL)
 		front = rear = newNode;
 	else
@@ -33,54 +31,31 @@ void enqueue1(int val)
 		rear = newNode;
 }
 
-void enqueue(int val)
-{
-	struct node *newNode, *temp;
-
-	newNode = malloc(sizeof(struct node));
-	if (newNode == NULL)
-		return;
-
-	newNode->data = val;
-	newNode->next = NULL;
-
-	if (head == NULL)
-		head = newNode;
-	else
-	{
-		temp = head;
-		while (temp->next != NULL)
-			temp = temp->next;
-
-		temp->next = newNode;
-	}
-}
 
 /**
  * dequeue - Removes element from the queue
  *
  * Return: -1 if list is empty otherwise the popped element
  */
-
-int dequeue()
+void dequeue()
 {
-	int data;
+	struct node *temp;
 
-	if (head ==  NULL)
-	{
-		printf("Stack Underflow: ");
-		return (-1);
-	}
+	if (front == NULL)
+		printf("Queue is empty. Unable to perform dequeue\n");
 	else
 	{
-		data =  head->data;
-		struct node *temp = head;
-		head = head->next;
+	    printf("Dequeue element = %d\n", front->data);
+
+		temp = front;
+		front = front->next;
+
+		if (front == NULL)
+		    rear = NULL;
+
 		free(temp);
 	}
-	return (data);
 }
-
 
 
 /**
@@ -94,10 +69,10 @@ int main(void)
 	enqueue(10);
 	enqueue(20);
 	enqueue(30);
-	printf("%d\n", dequeue());
-	printf("%d\n", dequeue());
-	printf("%d\n", dequeue());
-	printf("%d\n", dequeue());
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
 
 	return (0);
 }
