d7e57825803c2c9aa281528eba7ff0384ceec191
Your Namebelovetech <belovetech@gmail.com>
2022-08-17 05:17:12 +0000

    Check if edge available in adjacency matrix

--- data_structure/graph/adjacency_matrix/adjacency_matrix.c
+++ data_structure/graph/adjacency_matrix/adjacency_matrix.c
@@ -4,7 +4,7 @@
 
 /**
  * init - Initializes adjacency matrix to zero (0)
- * 
+ *
  * @arr: Adjacency matrix array
  */
 
@@ -19,12 +19,12 @@ void init(int arr[][v])
 
 /**
  * addEdge - Adds edge of adjacency matrix
- * 
+ *
  * @arr: Adjacency matrix array
  * @src: source of edge in adjacency matrix
  * @des: destination edge in adjacency matrix
  */
- 
+
 void addEdge(int arr[][v], int src, int des)
 {
     arr[src][des] = 1;
@@ -32,7 +32,7 @@ void addEdge(int arr[][v], int src, int des)
 
 /**
  * printAdjMatrix - Prints adjacency matrix
- * 
+ *
  * @arr: Adjacency matrix array
  */
 
@@ -48,10 +48,34 @@ void printAdjMatrix(int arr[][v])
     }
 }
 
+/**
+ * hasEdge - Checks if edge available in adjacency matrix
+ *
+ * @arr: Adjacency matrix array
+ * @src: source of edge in adjacency matrix
+ * @des: destination edge in adjacency matrix
+ * 
+ * @Return: 1 if found otherwise 
+ */
+int hasEdge(int arr[][v], int src, int des)
+{
+    if (arr[src][des] == 1)
+        return (1);
+
+    return (0);
+}
+
+void printHasEdge(int value)
+{
+    if (value)
+        printf("Edge found\n");
+    else
+        printf("No edge found\n");
+}
 
 /**
  * main: Implentation of direct adjacency matrix
- * 
+ *
  * @Return: 0 always (success)
  */
 int main(void)
@@ -67,7 +91,7 @@ int main(void)
     addEdge(adjMatrix, 2, 3);
     addEdge(adjMatrix, 3, 4);
 
-
+    printHasEdge(hasEdge(adjMatrix, 0, 1));
     printAdjMatrix(adjMatrix);
     return (0);
 }
