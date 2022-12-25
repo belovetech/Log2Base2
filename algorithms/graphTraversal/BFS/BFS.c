524dc9e6d0be04d3613602dbaf91acfd3bf6f1da
belovetech <belovetech@gmail.com>
2022-12-25 09:38:34 +0100

    [Update]: add documentation

--- algorithms/graphTraversal/BFS/BFS.c
+++ algorithms/graphTraversal/BFS/BFS.c
@@ -10,21 +10,26 @@
  */
 void BFS(int arr[][V], int source)
 {
+    // Initialize isVisited array to 0
     int isVisited[V] = {0};
     int index;
     
+    // Enqueue the first node into queue and mark it as visited
     enqueue(source);
     isVisited[source] = 1;
     
     while (!isQueueEmpty())
     {
+        // Dequeue the first node in the queue and print
         int node = dequeue();
         printf("Visited Node: %d\n", node);
         
+        // Check its neigbhors and mark them as visited
         for (index = 0; index < V; index++)
         {
             if (arr[node][index] == 1 && isVisited[index] == 0)
             {
+                // Enqueue its neigbhors into queue and mark it as visited
                 enqueue(index);
                 isVisited[index] = 1;
             }
--- algorithms/graphTraversal/README.md
+++ algorithms/graphTraversal/README.md
@@ -1,1 +1,9 @@
 # Graph Traversal
+
+#### BFS
+
+Breadth First Search (BFS) - Uses queue data structure
+
+#### DFS
+
+Depth First Search (DFS) - Uses stack data structure
