fb2bbf6cc6ae6c01d2ba39e5b2a9f7d69d623835
belovetech <belovetech@gmail.com>
2023-02-16 22:56:51 +0100

    [Feat]: check if two BTS are identical or not

---
+++ problem_solving_with_DSA/BTS/C/identicalTree.c
@@ -0,0 +1,25 @@
+#include "BST.h"
+
+/*
+** Definition for a Binary Tree
+** struct node
+** {
+**     int data;
+**     struct node *left;
+**     struct node *right;
+** };
+ */
+
+/**
+ * identicalTree - Check if the two BTS are equal
+ *
+ * @a: BTS 1
+ * @b: BTS 2
+ * Return: 1 if equal otherwise 0
+ */
+
+int identicalTree(struct node* a, struct node* b)
+{
+
+
+}
