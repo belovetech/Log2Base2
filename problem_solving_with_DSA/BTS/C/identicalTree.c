9fd7d6ac58f963f23725643e53524a81f6fe088c
belovetech <belovetech@gmail.com>
2023-02-16 23:34:35 +0100

    [Update]: Check if two BTS are equal

--- problem_solving_with_DSA/BTS/C/identicalTree.c
+++ problem_solving_with_DSA/BTS/C/identicalTree.c
@@ -1,15 +1,5 @@
 #include "BST.h"
 
-/*
-** Definition for a Binary Tree
-** struct node
-** {
-**     int data;
-**     struct node *left;
-**     struct node *right;
-** };
- */
-
 /**
  * identicalTree - Check if the two BTS are equal
  *
@@ -18,8 +8,42 @@
  * Return: 1 if equal otherwise 0
  */
 
-int identicalTree(struct node* a, struct node* b)
+int identicalTree(struct node *a, struct node *b)
 {
+	if (a == NULL && b == NULL)
+		return (1);
+	else if (a != NULL && b != NULL)
+	{
+		return (a->data == b->data &&
+				identicalTree(a->left, b->left) &&
+				identicalTree(a->right, b->right));
+	}
+	else
+		return (0);
+}
+
+/**
+ * main -Entry point
+ *
+ * Return: 1 (success) otherwise 0
+ */
+int main(void)
+{
+	struct node *a = NULL;
+	struct node *b = NULL;
+
+	a = insertNode(a, 100);
+	a = insertNode(a, 50);
+	a = insertNode(a, 200);
+	a = insertNode(a, 10);
+
+	b = insertNode(b, 100);
+	b = insertNode(b, 50);
+	b = insertNode(b, 200);
+	b = insertNode(b, 10);
 
 
+	printf("%d\n", identicalTree(a, b));
+
+	return (0);
 }
