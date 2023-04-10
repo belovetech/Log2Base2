eded61a0d16821ba3d973bf81422558f2723390c
belovetech <belovetech@gmail.com>
2023-04-10 10:30:28 +0100

    [Feat]: check for a loop in a linked list

--- coding_interview_pattern/fast_and_slow_pointers/hasLoop.c
+++ coding_interview_pattern/fast_and_slow_pointers/hasLoop.c
@@ -1,13 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-/*
-** Definition for a linked list node.
-*/
-struct node
-{
-   int data;
-   struct node *next;
-};
+#include "fast_slow.h"
 
 /**
  * hasLoop - Check if a linked list has a loop or not
@@ -46,38 +37,4 @@ void printNode(struct node *head)
         temp = temp->next;
     }
     printf("\n");
-}
-
-
-int main(void)
-{
-    struct node *h1 = malloc(sizeof(struct node));
-    struct node *h2 = malloc(sizeof(struct node));
-    struct node *h3 = malloc(sizeof(struct node));
-    struct node *h4 = malloc(sizeof(struct node));
-    struct node *h5 = malloc(sizeof(struct node));
-
-    h1->data = 10;
-    h2->data = 20;
-    h3->data = 30;
-    h4->data = 40;
-    h5->data = 50;
-
-    h1->next = h2;
-    h2->next = h3;
-    h3->next = h4;
-    h4->next = h5;
-    h5->next = h3;
-
-    // printNode(h1);
-
-    int res = hasLoop(h1);
-
-    if (res == 1)
-        printf("Linked list has a loop\n");
-    else
-        printf("Linked list does not has a loop\n");
-
-
-    return (0);
 }
